61a332a8ba4015083ae0e76cb4ee296315bf2bab
Nobita9 <nobitajp9@proton.me>
2022-11-04 00:36:26 +0700

    Update bet and star ticket

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -2,111 +2,195 @@ pragma solidity ^0.8.4;
 
 import '@openzeppelin/contracts/utils/Counters.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
-import "@openzeppelin/contracts/utils/Strings.sol";
+import '@openzeppelin/contracts/utils/Strings.sol';
 import '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
-
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 contract Bet is Ownable {
     using ECDSA for bytes32;
     using Counters for Counters.Counter;
 
+    uint256 constant FEE = 2; // 2/10000 = 0.02%;
+
+    uint8 constant NOT_END = 0;
+    uint8 constant A_WIN = 1;
+    uint8 constant DRAW = 2;
+    uint8 constant B_WIN = 3;
+
+    uint8 constant FIRST_HALF = 0;
+    uint8 constant SECOND_HALF = 1;
+    uint8 constant FULLTIME = 2;
+
     Counters.Counter public matchIds;
     Counters.Counter public betIds;
-
-    enum MatchResult {
-        A_WIN,
-        DRAW,
-        B_WIN
-    }
-
-    struct Odds {
-        uint256 aWin;
-        uint256 draw;
-        uint256 bWin;
-    }
+    address treasury;
+    address agbToken;
+    address starTicket;
+    mapping(uint256 => MatchInfo) public idToMatchInfo;
+    mapping(uint256 => BetInfo) public idToBetInfo;
 
     struct MatchInfo {
         uint256 itemId;
         string matchCode;
-        uint256 scoreA;
-        uint256 scoreB;
-        bool isStopBet;
+        uint8 firstHalfResult;
+        uint8 secondHalfResult;
+        uint8 fulltimeResult;
     }
     struct BetInfo {
         uint256 betId;
         uint256 matchId;
-        MatchResult bet;
-        Odds odds;
+        uint8 betType;
+        uint8 betResult;
+        uint16 oddsBet; // 2/100 ~ 0.02
         uint256 amount;
-        uint256 reward;
         bool isClaim;
     }
 
-     mapping(uint256 => MatchInfo) public idToMatchInfo;
-     mapping(uint256 => mapping(uint256 => BetInfo)) public idToBetInfo;
-
-    constructor() {
+    constructor(
+        address _treasury,
+        address _agbToken,
+        address _starTicket
+    ) {
+        treasury = _treasury;
+        agbToken = _agbToken;
+        starTicket = _starTicket;
     }
 
-    function createMatch(string memory _matchCode) public onlyOwner{
+    function createMatch(string memory _matchCode) public onlyOwner {
         uint256 itemId = matchIds.current();
-        idToMatchInfo[itemId] = MatchInfo(
-            itemId,
-            _matchCode,
-            0,
-            0,
-            false
-        );
+        idToMatchInfo[itemId] = MatchInfo(itemId, _matchCode, 0, 0, 0);
         matchIds.increment();
+        // Event here
     }
 
-    function updateMatch(
-        uint256 _matchId,
-        uint256 _scoreA,
-        uint256 _scoreB
-    ) public onlyOwner{
-        idToMatchInfo[_matchId].scoreA = _scoreA;
-        idToMatchInfo[_matchId].scoreB = _scoreB;
-        idToMatchInfo[_matchId].isStopBet = true;
+    function updateFirstHalf(uint256 _matchId, uint8 _result) public onlyOwner {
+        MatchInfo memory matchInfo = idToMatchInfo[_matchId];
+        require(matchInfo.firstHalfResult == NOT_END, 'First half was updated');
+        idToMatchInfo[_matchId].firstHalfResult = _result;
+        // Event here
     }
 
+    function updateSecondHalf(uint256 _matchId, uint8 _result)
+        public
+        onlyOwner
+    {
+        MatchInfo memory matchInfo = idToMatchInfo[_matchId];
+        require(
+            matchInfo.firstHalfResult != NOT_END,
+            'First half was not updated'
+        );
+        require(
+            matchInfo.secondHalfResult == NOT_END,
+            'Second half was updated'
+        );
+        idToMatchInfo[_matchId].secondHalfResult = _result;
+        // Event here
+    }
+
+    function updateFulltime(uint256 _matchId, uint8 _result) public onlyOwner {
+        MatchInfo memory matchInfo = idToMatchInfo[_matchId];
+        require(
+            matchInfo.firstHalfResult != NOT_END,
+            'First half was not updated'
+        );
+        require(
+            matchInfo.secondHalfResult != NOT_END,
+            'Second half was not updated'
+        );
+        require(matchInfo.fulltimeResult == NOT_END, 'Fulltime was updated');
+        idToMatchInfo[_matchId].secondHalfResult = _result;
+        // Event here
+    }
 
     function userBet(
         uint256 _matchId,
-        MatchResult _bet,
+        uint8 _betType,
         uint256 _amount,
-        Odds memory _odds,
-        uint256 _reward,
+        uint16 _oddsBet,
+        uint8 _betResult,
         bytes memory _hashedMessage
-    ) public{
-        bool isValid = verifyMessage(_reward, _hashedMessage);
-        require(isValid);
+    ) public {
+        MatchInfo memory matchInfo = idToMatchInfo[_matchId];
+        bool isValidBet = true;
+        if (_betType == FIRST_HALF && matchInfo.firstHalfResult != NOT_END) {
+            isValidBet = false;
+        }
+        if (_betType == SECOND_HALF && matchInfo.secondHalfResult != NOT_END) {
+            isValidBet = false;
+        }
+        if (_betType == FULLTIME && matchInfo.fulltimeResult != NOT_END) {
+            isValidBet = false;
+        }
+        require(isValidBet, "This bet infomation is invalid");
+        // require(!idToMatchInfo[_matchId].isStopBet, 'This match was ended');
+        isValidBet = verifyMessage(
+            _matchId,
+            _betType,
+            _amount,
+            _oddsBet,
+            _hashedMessage
+        );
+        require(isValidBet, "This bet signature is invalid");
         uint256 itemId = betIds.current();
-        idToBetInfo[itemId][_matchId] = BetInfo(
+        idToBetInfo[itemId] = BetInfo(
             itemId,
             _matchId,
-            _bet,
-            _odds,
+            _betType,
+            _betResult,
+            _oddsBet,
             _amount,
-            _reward,
             false
         );
         betIds.increment();
+        IERC20(agbToken).transferFrom(msg.sender, address(this), _amount);
+        // Event here
     }
 
-    function userClaim(uint256 betId) public {
-        // claim logic
-    }
-
-    function verifyMessage(uint256 reward, bytes memory signature) public view  returns(bool) {
-        bytes32 messageHash =  keccak256(bytes(Strings.toString(reward)));
-       
-        address signerAddress = messageHash.toEthSignedMessageHash().recover(signature);
-              
-        if (signerAddress==owner()) {
-            return true;
-        } else {
-            return false;
+    function userClaim(uint256 _betId) public {
+        BetInfo storage betInfo = idToBetInfo[_betId];
+        MatchInfo memory matchInfo = idToMatchInfo[betInfo.matchId];
+        bool isValidClaim = true;
+        if (betInfo.betType == FIRST_HALF && matchInfo.firstHalfResult == NOT_END) {
+            isValidClaim = false;
         }
+        if (betInfo.betType == SECOND_HALF && matchInfo.secondHalfResult == NOT_END) {
+            isValidClaim = false;
+        }
+        if (betInfo.betType == FULLTIME && matchInfo.fulltimeResult == NOT_END) {
+            isValidClaim = false;
+        }
+        require(isValidClaim, "This claim infomation is invalid");
+        require(!betInfo.isClaim, 'User claimed');
+        bool isWin = false;
+        if (betInfo.betType == FIRST_HALF && betInfo.betResult == matchInfo.firstHalfResult) {
+            isWin = true;
+        }
+        if (betInfo.betType == SECOND_HALF && betInfo.betResult == matchInfo.secondHalfResult) {
+            isWin = true;
+        }
+        if (betInfo.betType == FULLTIME && betInfo.betResult == matchInfo.fulltimeResult) {
+            isWin = true;
+        }
+        require(isWin, 'You lose');
+        betInfo.isClaim = true;
+        uint256 reward = (betInfo.oddsBet * betInfo.amount) / 100;
+        IERC20(agbToken).transfer(msg.sender, reward);
+        // Event here
+    }
+
+    function verifyMessage(
+        uint256 _matchId,
+        uint8 _betType,
+        uint256 _amount,
+        uint256 _oddsBet,
+        bytes memory signature
+    ) public view returns (bool) {
+        bytes32 messageHash = keccak256(
+            abi.encodePacked(_matchId, _betType, _amount, _oddsBet)
+        );
+        address signerAddress = messageHash.toEthSignedMessageHash().recover(
+            signature
+        );
+        return signerAddress == owner();
     }
 }
--- contracts/StarTicket.sol
+++ contracts/StarTicket.sol
@@ -5,9 +5,51 @@ import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract StarTicket is ERC1155, Ownable {
-    constructor(string memory _uri) ERC1155(_uri) {}
+    uint8 constant TICKET_1 = 1;
+    uint8 constant TICKET_2 = 2;
+    uint8 constant TICKET_3 = 3;
+    uint8 constant TICKET_4 = 4;
+    uint8 constant TICKET_5 = 5;
+    uint8 constant TICKET_6 = 6;
 
-    function mint(address _to, uint256 _tokenId, uint256 _amount, bytes memory _data) public onlyOwner {
+    mapping(uint8 => uint16) bonusProfit;
+
+    constructor(string memory _uri) ERC1155(_uri) {
+        bonusProfit[TICKET_1] = 3; // 3/100 = 3%
+        bonusProfit[TICKET_2] = 5; // 5/100 = 5%
+        bonusProfit[TICKET_3] = 10; // 10/100 = 10%
+        bonusProfit[TICKET_4] = 15; // 15/100 = 15%
+        bonusProfit[TICKET_5] = 20; // 20/100 = 20%
+        bonusProfit[TICKET_6] = 0; // random 20<x<=50
+    }
+
+    function mint(
+        address _to,
+        uint256 _tokenId,
+        uint256 _amount,
+        bytes memory _data
+    ) public onlyOwner {
         _mint(_to, _tokenId, _amount, _data);
+    }
+
+    function getBonusProfit(uint8 _ticketId) public view returns (uint16) {
+        require(
+            balanceOf(msg.sender, _ticketId) > 0,
+            'User do not have this ticket'
+        );
+        uint16 bonus = bonusProfit[_ticketId];
+        if (bonus == bonusProfit[TICKET_6]) {
+            bonus = random();
+        }
+        return bonus;
+    }
+
+    function random() private view returns (uint16) {
+        uint256 randomNumber = uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, block.number, block.coinbase)));
+        uint16 firstFandom = uint16(randomNumber % 50);
+        if (firstFandom < 20) {
+            return uint16(firstFandom + (randomNumber % 20));
+        }
+        return firstFandom;
     }
 }
